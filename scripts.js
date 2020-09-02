23d8953a2be6b4195413ae4ae4b7dd825c16861f
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-02 11:55:38 -0400

    testing lazy load for galleries

--- scripts.js
+++ scripts.js
@@ -12,12 +12,8 @@ $(document).ready(function() {
 			navigateByImgClick: true,
 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
 		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
 	});
 });
+
+// Lazy load effect for galleries 
 
