197703b368fc23b7d024e9f4150c30ff51c9020a
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-11 16:30:29 -0400

    removed pop-up gallery coding in scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,3 @@
-
-// Pop-up Gallery and Slideshow code
-
-$(document).ready(function() {
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-	});
-});
-
-// Lazy load effect for galleries 
-
-
-
 // AJAX for contact form submissions
 
   window.addEventListener("DOMContentLoaded", function() {
