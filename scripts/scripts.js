6c1b1f8026afa7edcf8b1d3abf537335df221f02
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-22 17:39:15 -0400

    adjusted diamonds on Photgraphy Styles section of services.html, fixed margins around portfolio section of index.html, removed fade in effect for index.htmll parallax

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -86,7 +86,7 @@ AOS.init({
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 10, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
+  delay: 50, // values from 0 to 3000, with step 50ms
   duration: 100, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: true, // whether animation should happen only once - while scrolling down
